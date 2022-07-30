bb405064d352ad506376dee07371ac58144ac5c9
13prashant <pacific1392@gmail.com>
2022-07-30 21:29:49 +0530

    created Welcome page, added firebase auth with google provider

--- src/App.js
+++ src/App.js
@@ -3,27 +3,27 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 // Pages
 import Welcome from './pages/welcome/Welcome';
-import Login from './pages/login/Login';
-import Signup from './pages/signup/Signup';
 import Lyrics from './pages/lyrics/Lyrics';
 import Concerts from './pages/concerts/Concerts';
 import Concert from './pages/concert/Concert';
 import CreateConcert from './pages/createConcert/CreateConcert';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { user } = useAuthContext();
+  console.log(user);
+
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route element={<Layout />}>
           <Route path="/lyrics" element={<Lyrics />} />
           <Route path="/concerts" element={<Concerts />} />
           <Route path="/concerts/:concert" element={<Concert />} />
           <Route path="/concerts/create-concert" element={<CreateConcert />} />
-        </Routes>
-      </Layout>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/pages/welcome/Welcome.js
+++ src/pages/welcome/Welcome.js
@@ -1,5 +1,75 @@
+import { useNavigate } from 'react-router-dom';
+// Hooks
+import { useLogin } from '../../hooks/useLogin';
+// Material components
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+
+// Material Icons
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookIcon from '@mui/icons-material/Facebook';
+// Constants
+import { APP_NAME } from '../../utils/constants';
+
 const Welcome = () => {
-  return <div>Welcome</div>;
+  const navigate = useNavigate();
+  const { login } = useLogin();
+
+  return (
+    <Box
+      display="flex"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Paper
+        sx={{
+          maxWidth: '25rem',
+          textAlign: 'center',
+          padding: '2rem',
+        }}
+        variant="outlined"
+      >
+        <Typography variant="h5" mb={2}>
+          {APP_NAME}
+        </Typography>
+        <Typography variant="h6">Sign In</Typography>
+        <Typography variant="p">
+          to continue to access concerts' details
+        </Typography>
+        <Button
+          variant="outlined"
+          startIcon={<GoogleIcon />}
+          fullWidth
+          sx={{ margin: '2rem 0 1rem 0' }}
+          onClick={login}
+        >
+          Sign In with Google
+        </Button>
+        <br />
+        <Button
+          variant="outlined"
+          startIcon={<FacebookIcon />}
+          fullWidth
+          sx={{ margin: '0 0 2rem 0' }}
+        >
+          Sign In with Facebook
+        </Button>
+        <Typography variant="h6" mb={2}>
+          OR
+        </Typography>
+        <Typography variant="a">
+          Continue to{' '}
+          <Link onClick={() => navigate('/lyrics')} sx={{ cursor: 'pointer' }}>
+            FIND SONGS' LYRICS
+          </Link>
+        </Typography>
+      </Paper>
+    </Box>
+  );
 };
 
 export default Welcome;
