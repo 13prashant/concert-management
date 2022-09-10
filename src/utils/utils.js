46220d1aa4a6598ff43812a615e6717e48dae57c
Nishant Chavda <nishantchavda36@gmail.com>
2022-09-10 13:50:21 -0400

    fixed breakLyricsArrayByAlphabets

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -29,28 +29,47 @@ export const uniqueId = () => {
   return Math.floor(Math.random() * 100) * Date.now();
 };
 
-export const breakLyricsArrayByAlphabets = (lyricsArr) => {
-  let result = [];
+export const breakLyricsArrayByAlphabets = (lyrics) => {
+  //Lyrics array must be sorted
+  lyrics.sort((a, b) => {
+    if (a.fileName.toLowerCase() < b.fileName.toLowerCase()) return -1;
+    if (a.fileName.toLowerCase() > b.fileName.toLowerCase()) return 1;
+    return 0;
+  });
+  let res = [];
+  let firstNameFirstChar = [];
 
-  let alphabeticalArr = [lyricsArr[0]];
+  //Pointers
+  let p1 = 0;
+  let p2 = 1;
+  let p3 = 0;
 
-  for (let i = 1; i < lyricsArr.length; i++) {
-    let lyrics = lyricsArr[i];
-    let fileNameFirstChar = lyrics.fileName.charAt(0).toLowerCase();
+  //Create array based on first character of filename
+  for (let ele of lyrics) {
+    firstNameFirstChar.push(ele.fileName.charAt(0).toLowerCase());
+  }
 
-    let fileNameFirstCharOfLastLyricsOfAlphabeticalArr = alphabeticalArr[
-      alphabeticalArr.length - 1
-    ].fileName
-      .charAt(0)
-      .toLowerCase();
+  //Create arrays inside array based on simliar char of fileName
+  while (p2 <= firstNameFirstChar.length) {
+    if (firstNameFirstChar[p1] === firstNameFirstChar[p2]) {
+      p2++;
+    }
 
-    if (fileNameFirstChar !== fileNameFirstCharOfLastLyricsOfAlphabeticalArr) {
-      result.push(alphabeticalArr);
-      alphabeticalArr = [lyricsArr[i]];
-      result.push(alphabeticalArr);
-    } else {
-      alphabeticalArr.push(lyricsArr);
+    if (firstNameFirstChar[p1] !== firstNameFirstChar[p2]) {
+      res.push(new Array(p2 - p1).fill(0));
+      p1 = p2;
+      p2++;
     }
   }
-  return result;
+  //Assign object to result array
+  for (let ele of res) {
+    for (let j = 0; j < ele.length; j++) {
+      ele[j] = lyrics[p3];
+      p3++;
+      if (p3 === lyrics.length) {
+        break;
+      }
+    }
+  }
+  return res;
 };
